06a85dcd60a5f800d1b5b3667c70f08ec1386630
Colton <ct.earnest@yahoo.com>
2023-05-05 05:27:15 -0500

    Activity Indicator

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import Main from "./screens/MainComponent";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
-import { Store } from "./redux/store";
+import { store } from "./redux/store";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-          <NavigationContainer>
-              <Main />
-          </NavigationContainer>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Main />
+            </NavigationContainer>
+        </Provider>
+    );
 }
-
-
